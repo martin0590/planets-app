a570956450af0e4e1f6119712a8c87240aaa4421
Martin M <martin.ale590@gmail.com>
2025-11-12 16:03:05 -0300

    added - cors & planets mvc

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
 
 // Middlewares
+app.use(cors({
+    origin: process.env.ORIGIN
+}))
 app.use(express.json())
+app.use(planetsRouter)
 
 module.exports = app

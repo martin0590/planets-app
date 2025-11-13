9eafe19bd43662f3f0380e0939ecb2940a96896f
Martin M <martin.ale590@gmail.com>
2025-11-13 16:12:52 -0300

    fixed - error with npm build & route

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/planets', planetsRouter)
 app.use('/launches', launchesRouter)
 
-app.get('/*', (req, res) => {
+app.get('/*splat', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 

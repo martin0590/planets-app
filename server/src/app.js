84c12362594b93e68c2df077bee6e92ef8436524
Martin M <martin.ale590@gmail.com>
2025-11-13 14:32:16 -0300

    added - launchs mvc

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
+
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launcher.router')
 
 const app = express()
 
@@ -8,7 +12,16 @@ const app = express()
 app.use(cors({
     origin: process.env.ORIGIN
 }))
+app.use(morgan('combined'))
+
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter)
+app.use(launchesRouter)
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app

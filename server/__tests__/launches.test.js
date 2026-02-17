450eeedaa71c2138c62e1eb97b7acb232198e5e8
Martin M <martin.ale590@gmail.com>
2026-02-17 14:44:08 -0300

    added - github actions file

--- server/__tests__/launches.test.js
+++ server/__tests__/launches.test.js
@@ -1,10 +1,12 @@
 const request = require("supertest");
 const app = require("../src/app");
 const { mongoConnect, mongoDisconnect } = require("../src/services/mongo");
+const { loadPlanetsData } = require("../src/models/planets.model");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
